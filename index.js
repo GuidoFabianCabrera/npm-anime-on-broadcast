fafaa847e1d2df5818f6eb4a283293372740f211
Guido Fabian Cabrera <guidofabiancabrera@hotmail.com>
2022-08-07 01:58:32 -0300

    change cloudscraper by puppeteer

--- index.js
+++ index.js
@@ -1,89 +1,125 @@
-const cloudscraper = require('cloudscraper');
+const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 const _ = require('lodash');
 let fs = require('fs');
 
-const getDate = async (urlItem) => {
-  const url = urlItem;
-  const res = await cloudscraper(url, { method: 'GET' });
-  const $ = cheerio.load(res);
+const getList = async (url) => {
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const [page] = await browser.pages();
 
-  // ----- function to get variables from the DOM
+    await page.goto(url);
 
-  const findTextAndReturnRemainder = (target, variable) => {
-    const chopFront = target.substring(
-      target.search(variable) + variable.length,
-      target.length
-    );
+    await page.waitForSelector('div#mCSB_1_container > ul.ListSdbr');
 
-    const result = chopFront.substring(0, chopFront.search(';'));
-
-    return result;
-  };
-
-  // ----- get var anime_info for date
-
-  const text = await $($('script')).text();
-  const findAndClean = await findTextAndReturnRemainder(
-    text,
-    'var anime_info ='
-  );
-  const result = await JSON.parse(findAndClean);
-
-  // ----- get image
-
-  const image = await $('div.Image > figure > img').attr('src');
-
-  return {
-    date: result[result.length - 1],
-    image: 'https://www3.animeflv.net' + image,
-  };
-};
-
-const getList = async () => {
-  const url = 'https://www3.animeflv.net';
-  const res = await cloudscraper(url, { method: 'GET' });
-  const $ = cheerio.load(res);
-
-  const list = [];
-
-  $('ul.ListSdbr')
-    .find('li > a')
-    .each((index, element) => {
-      // ----- get anime name
-
-      const name = $(element)
-        .contents()
-        .filter(function () {
-          return this.type === 'text';
-        })
-        .text();
-
-      // ----- get anime url
-
-      const itemUrl = url + $(element).attr('href');
-
-      list.push({
-        name: name.slice(0, -1),
-        url: itemUrl,
-      });
+    const pageData = await page.evaluate(() => {
+      return {
+        html: document.documentElement.innerHTML,
+      };
     });
 
-  return list;
+    const $ = cheerio.load(pageData.html);
+
+    const list = [];
+
+    $('ul.ListSdbr')
+      .find('li > a')
+      .each((index, element) => {
+        // ----- get anime name
+
+        const name = $(element)
+          .contents()
+          .filter(function () {
+            return this.type === 'text';
+          })
+          .text();
+
+        // ----- get anime url
+
+        const itemUrl = url + $(element).attr('href');
+
+        list.push({
+          name: name.slice(0, -1),
+          url: itemUrl,
+        });
+      });
+
+    await browser.close();
+
+    return list;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const getDate = async (urlItem) => {
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const [page] = await browser.pages();
+
+    await page.goto(urlItem);
+
+    await page.waitForSelector('span.Date.fa-calendar');
+
+    const pageData = await page.evaluate(() => {
+      return {
+        html: document.documentElement.innerHTML,
+      };
+    });
+
+    const $ = cheerio.load(pageData.html);
+
+    // ----- function to get variables from the DOM
+
+    const findTextAndReturnRemainder = (target, variable) => {
+      const chopFront = target.substring(
+        target.search(variable) + variable.length,
+        target.length
+      );
+
+      const result = chopFront.substring(0, chopFront.search(';'));
+
+      return result;
+    };
+
+    // ----- get var anime_info for date
+
+    const text = $($('script')).text();
+    const findAndClean = await findTextAndReturnRemainder(
+      text,
+      'var anime_info ='
+    );
+    const result = await JSON.parse(findAndClean);
+
+    // ----- get image
+
+    const image = $('div.Image > figure > img').attr('src');
+
+    await browser.close();
+
+    return {
+      date: result[result.length - 1],
+      image: 'https://www3.animeflv.net' + image,
+    };
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 (async () => {
+  const url = 'https://www3.animeflv.net';
+
   console.log(
     '\n---------------------------------------- initialized ----------------------------------------\n'
   );
 
   // ----- get list of anime on broadcast
-  const list = await getList();
+  const list = await getList(url);
 
   if (list) console.log('-list obtained\n');
 
   // ----- list cut by chunk
-  const chunkList = _.chunk(list, 10);
+  const chunkList = _.chunk(list, 5);
 
   // ----- resolve list
   for await (const row of chunkList) {
